1000ab53e0d664dffc5fa31db7db5cce1fad9c61
Vladmir-JJ <jjwysocki89@gmail.com>
2023-10-27 01:11:19 +0200

    added: loadable data type for json serialization

---
+++ NmeaParserConsole/LoadableData.cs
@@ -0,0 +1,42 @@
+﻿using NmeaParserConsole.MessageData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NmeaParserConsole
+{
+    [Serializable]
+    internal class LoadableData
+    {
+        [Serializable]
+        public struct FieldCharacteristics
+        {
+            public  string Description;
+            public  string FieldType;
+            public  string Format;
+
+            [JsonConstructor]
+            public FieldCharacteristics(string Description, string FieldType, string Format)
+            {
+                this.Description = Description;
+                this.FieldType = FieldType;
+                this.Format = Format;
+            }
+
+        }
+
+        public string Header;
+        public List<FieldCharacteristics> RequiredFields;
+
+        [JsonConstructor]
+        public LoadableData(string Header, List<FieldCharacteristics> RequiredFields)
+        {
+            this.Header = Header;
+            this.RequiredFields = RequiredFields;
+
+        }
+    }
+}
