e472c3716f64cfd3d4ea8672e7bc9f36c78ba65b
Vladmir-JJ <jjwysocki89@gmail.com>
2023-10-27 01:14:17 +0200

    added: factory for populating nmea data holder with proper fields

---
+++ NmeaParserConsole/NmeaFieldFactory.cs
@@ -0,0 +1,31 @@
+﻿using NmeaParserConsole.MessageData;
+using System.Globalization;
+
+namespace NmeaParserConsole
+{
+    internal class NmeaFieldFactory
+    {
+        public IPrintableData? CreateField(LoadableData.FieldCharacteristics fieldInfo, string fieldValue)
+        {
+            string fieldType = fieldInfo.FieldType;
+            if (fieldType == typeof(float).Name)
+            {
+                var value = float.Parse(fieldValue, CultureInfo.InvariantCulture);
+                return new FieldTypeFloat(value, fieldInfo.Description);
+            }
+            if (fieldType == typeof(char).Name)
+            {
+                var c = char.Parse(fieldValue);
+                return new FieldTypeChar(c, fieldInfo.Description);
+            }
+
+            Console.WriteLine($"[{this}] no valid field type found for type {fieldInfo.FieldType}");
+            return null;
+        }
+
+        private void LogError(string fieldValue, string fieldType)
+        {
+            Console.WriteLine($"[{this}] parsing of {fieldValue} to {fieldType} failed");
+        }
+    }
+}
