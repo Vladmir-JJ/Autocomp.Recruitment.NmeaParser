5b54a131fdc38be994c3d543d58aa77ff0804633
Vladmir-JJ <jjwysocki89@gmail.com>
2023-10-26 19:22:01 +0200

    added: data factory

---
+++ NmeaParserConsole/NmeaDataFactory.cs
@@ -0,0 +1,33 @@
+﻿using Autocomp.Nmea.Common;
+using NmeaParserConsole.MessageData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NmeaParserConsole
+{   
+    public enum SupportetHeader
+    {
+        MWV,
+        GLL
+    }
+
+    internal class NmeaDataFactory
+    {      
+        public AbstractMessageData CreateMessageData(NmeaMessage message)
+        {          
+            string header = message.Header;
+            if (header == SupportetHeader.MWV.ToString())
+                return new MWVMessageData(message);
+            else if (header == SupportetHeader.GLL.ToString())
+                return new GLLMessageData(message);
+
+            //Add new Message header variants here
+
+            ErrorLogger.LogFactoryError(message, this);
+            return null;
+        }
+    }
+}
