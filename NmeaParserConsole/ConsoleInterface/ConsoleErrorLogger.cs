9b800d2c4cb912c5083ba351cce0f034caa9284f
Vladmir-JJ <jjwysocki89@gmail.com>
2023-10-28 23:42:52 +0200

    added: console error logger

---
+++ NmeaParserConsole/ConsoleInterface/ConsoleErrorLogger.cs
@@ -0,0 +1,19 @@
+﻿namespace NmeaParserConsole.ConsoleInterface
+{
+    public static class ConsoleErrorLogger
+    {        
+        public static void LogError(string reason)
+        {
+            Console.WriteLine(reason);
+        }
+
+        public static void LogError(object from, string reason, string arg)
+        {
+            Console.WriteLine($"[{from}] : {reason} {arg}");
+        }
+        public static void LogError(object from, string reason, string arg1, string arg2)
+        {
+            Console.WriteLine($"[{from}] : {reason}", arg1, arg2);
+        }
+    }
+}
