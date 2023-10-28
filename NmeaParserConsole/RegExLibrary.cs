3e93ff31e3fd65ed926424696fd954fcec518daa
Vladmir-JJ <jjwysocki89@gmail.com>
2023-10-28 17:43:35 +0200

    changed: regex library

--- NmeaParserConsole/RegExLibrary.cs
+++ NmeaParserConsole/RegExLibrary.cs
@@ -1,24 +1,19 @@
 ﻿namespace NmeaParserConsole
 {
-    internal static class RegExLibrary
-    {
-        // public const string EXP_FLOAT = @"^-?[0-9]\d*.\d*$";
-        // public const string EXP_INT = @"^-?[0-9]*$";   
-
-
-
-        //TODO: check docu if lowercase message indicator is acceptable (ie NS, Status, etc)
+    public static class RegExLibrary
+    {       
+        //Numeric:
         public const string FORMAT_NUMERIC = @"^\-?\d[0-9]*(\.[0-9]*)?$";
-        public const string FORMAT_LONG_LAT = @"^\d{1,4}(\.\d{1,2})?$";
-        public const string FORMAT_NS = @"^[n,N,s,S]$";
-        public const string FORMAT_EW = @"^[e,E,w,W]$";
-        public const string FORMAT_UTC = @"^[0-2]?\d[0-5]\d[0-5]\d(\.\d{1,2})?$";
-        public const string FORMAT_STATUS = @"[a,A,v,V]";
-        public const string FORMAT_MODE_INDICATOR = @"[a,A,d,D,e,E,m,M,s,S,n,N]";
-        public const string FORMAT_ANGLE = @"^[0-3]?\d?\d(\.\d{1,2})?$";
-        public const string FORMAT_RELATIVE = @"^[r,R,t,T]$";
-        public const string FORMAT_SPEED_UNITS = @"[k,K,m,M,n,N,s,S]";
-        //public readonly static string EXP_INT = @"^[0-9]*$";
+        public const string FORMAT_LONG_LAT = @"^\d{5}\.?(\d{1,2})?$";
+        public const string FORMAT_UTC = @"^([0-1][0-9]|2[0-3])[0-5]\d[0-5]\d(\.\d*)?$";
+        public const string FORMAT_ANGLE = @"^[0-3]?\d?\d(\.\d*)?$";
 
+        //Char type:
+        public const string FORMAT_NS = @"^[N,S]$";
+        public const string FORMAT_EW = @"^[E,W]$";
+        public const string FORMAT_STATUS = @"^[A,V]$";
+        public const string FORMAT_MODE_INDICATOR = @"^[A,D,E,M,S,N]$";
+        public const string FORMAT_RELATIVE = @"^[R,T]$";
+        public const string FORMAT_SPEED_UNITS = @"^[K,M,N,S]$"; 
     }
 }
